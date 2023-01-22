3e956cc0f5544d4bf8ef474e73725f9a4c04a801
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-01-22 19:44:18 +0100

    Updated item seed with image

--- pockets/models.py
+++ pockets/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Pocket(models.Model):
+
     number_of_red_packets = models.IntegerField(default=0)
     items = models.ManyToManyField('items.Item', related_name="pockets", blank=True)
     owner = models.ForeignKey("jwt_auth.User", related_name="pocket", on_delete=models.CASCADE)
