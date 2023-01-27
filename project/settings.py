7308df74daeda39ad637ba3924c2fe79cbbbe898
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-01-27 10:44:56 +0100

    Added link in settings

--- project/settings.py
+++ project/settings.py
@@ -154,6 +154,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://red-packet-clicker.netlify.app"
 ]
 
 django_on_heroku.settings(locals())
